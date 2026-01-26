fc960f0e3d30e2a32b5680bc1e30367bdbd340d8
Glueeed <swiderskikuba12@gmail.com>
2026-01-26 17:41:47 +0100

    style: Add github buttons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,11 +10,13 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 
     <title>Glueeed.dev</title>
 
     @vite('resources/css/app.css')
+
     @livewireStyles
 </head>
 <body class="antialiased bg-gray-950 text-gray-100 min-h-screen flex flex-col">
@@ -24,18 +26,18 @@
 
 
 
-<footer class=" h-60 mt-20 border-t border-violet-800/30 bg-gradient-to-b from-violet-950/20 to-indigo-950/10 ">
+<footer class=" h-70 mt-20 border-t border-violet-800/30 bg-gradient-to-b from-violet-950/20 to-indigo-950/10 ">
     <div class="container mx-auto px-6 z-10">
         <div class="flex flex-col md:flex-row justify-between items-center">
             <div class="mb-6 mt-15 md:mb-0">
                 <h3 class="text-violet-400 font-semibold tracking-wide">Glueeed.dev</h3>
-                <p class="mt-2 text-sm text-violet-200/70">Privacy‑first apps, zero‑knowledge i trochę magii.</p>
+                <p class="mt-2 text-sm text-violet-200/70 italic">glue and nonces in my dreams since forever</p>
             </div>
         </div>
         <div class="border-t text-xs text-violet-200/60 mt-10 p-6 text-center">
             <p class="font-bold">© {{ date('Y') }} Glueeed</p>
             <p> Wszelkie prawa zastrzeżone.</p>
-            <div class="mt-2 md:mt-0 font-mono">Laravel • Livewire • Tailwind</div>
+            <div class=" mt-5 font-mono">Laravel • Livewire • Tailwind</div>
         </div>
     </div>
 
